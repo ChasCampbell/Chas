6e99767c42d59d6240d42fd9f090d00e3cf4d2e1
Chas Campbell <Chas@MacBookAir.local>
2022-07-22 17:57:03 -0400

    Updates

---
+++ precache-manifest.3bf1e0bf8cbcd2ca124185815af94282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc7da841f4837e33795",
+    "url": "/static/css/main.bda95173.chunk.css"
+  },
+  {
+    "revision": "fdc7da841f4837e33795",
+    "url": "/static/js/main.9169efc0.chunk.js"
+  },
+  {
+    "revision": "e4686e9a92b21e222891",
+    "url": "/static/js/runtime~main.99c07c0c.js"
+  },
+  {
+    "revision": "b590cfa9c9644a5b7fe5",
+    "url": "/static/js/2.652321e8.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "c992dc69207845576770d556b8f25ef5",
+    "url": "/index.html"
+  }
+];
